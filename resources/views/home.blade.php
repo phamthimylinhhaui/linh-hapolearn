764019a8f490e2b213341b288978dc10d51afb0d
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-17 16:17:10 +0700

    #2 change header responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,22 +13,5 @@
         <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
     </div>
 </section>
-{{--<section class="example">--}}
-{{--    <div class="container">--}}
-{{--        <div class="row justify-content-center">--}}
-{{--            <div class="col-md-12">--}}
-{{--                <div class="card">--}}
-{{--                    <div class="card-header">{{ __('Dashboard') }}</div>--}}
-{{--                    <div class="card-body">--}}
-{{--                        Hello World!!!--}}
-{{--                    </div>--}}
 
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</section>--}}
-{{--<div class="hello">--}}
-{{--    đây là test thử--}}
-{{--</div>--}}
 @endsection
