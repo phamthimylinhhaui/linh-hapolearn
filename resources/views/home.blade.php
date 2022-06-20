e513d3c75caf0b273cb8a6eb6a955c4607796358
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-20 09:33:28 +0700

    modify header and banner responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
-<section class="banner">
-    <div class="content">
-        <div class="content-title-1 content-item">learn anytime, anywhere</div>
-        <div class="content-title-2 content-item">at HapoLearning
-            <img src="http://localhost/scaford-hapolearn/resources/assets/images/hapo.png">
-            !
+    <section class="banner">
+        <div class="container-banner">
+            <div class="content">
+                <div class="content-title-1 content-item">learn anytime, anywhere</div>
+                <div class="content-title-2">at HapoLearning
+                    <img src="http://localhost/linh-hapolearn/resources/assets/images/hapo.png">                    !
+                </div>
+                <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
+                <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
+            </div>
         </div>
-        <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
-        <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
-    </div>
-</section>
-
+        <div class="effect-banner"></div>
+    </section>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,7 @@
         <a class="navbar-brand" href="#">
             <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
         </a>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,18 +22,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">PROFILE</a>
                 </li>
-{{--                <li class="nav-item dropdown">--}}
-{{--                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                        LOGIN/REGISTER--}}
-{{--                    </a>--}}
-{{--                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
-{{--                        <a class="dropdown-item" href="#">Action</a>--}}
-{{--                        <a class="dropdown-item" href="#">Another action</a>--}}
-{{--                        <div class="dropdown-divider"></div>--}}
-{{--                        <a class="dropdown-item" href="#">Something else here</a>--}}
-{{--                    </div>--}}
-{{--                </li>--}}
-
             </ul>
         </div>
     </nav>
