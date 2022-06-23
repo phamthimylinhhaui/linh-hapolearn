085f4e0bcf2945192c4e27f4db22efb7e7641920
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-23 11:03:42 +0700

    #3: add attribute of image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="content">
                 <div class="content-title-1 content-item">learn anytime, anywhere</div>
                 <div class="content-title-2"><span>at HapoLearning</span>
-                    <img src="http://localhost/linh-hapolearn/resources/assets/images/hapo.png">                    !
+                    <img src="http://localhost/linh-hapolearn/resources/assets/images/hapo.png" alt="icon-hapo">                    !
                 </div>
                 <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
                 <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
