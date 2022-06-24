193b2e95cead766672ec21ba08a075ca17bfba33
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-24 16:23:05 +0700

    #3 change content banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="content">
                 <div class="content-item">learn anytime, anywhere</div>
                 <div class="content-title"><span>at HapoLearning</span>
-                    <img src="{{ asset('images/hapo.png') }}" alt="icon-hapo">                    !
+                    <img src="{{ asset('images/hapo.png') }}" alt="icon-hapo">!
                 </div>
                 <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
                 <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
