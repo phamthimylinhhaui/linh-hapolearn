4ed05164f77c99b13e2e33352e93f0f23201d83d
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-24 16:19:10 +0700

    #3 change banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
     <section class="banner">
         <div class="container-banner">
             <div class="content">
-                <div class="content-title-1 content-item">learn anytime, anywhere</div>
-                <div class="content-title-2"><span>at HapoLearning</span>
+                <div class="content-item">learn anytime, anywhere</div>
+                <div class="content-title"><span>at HapoLearning</span>
                     <img src="{{ asset('images/hapo.png') }}" alt="icon-hapo">                    !
                 </div>
                 <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
