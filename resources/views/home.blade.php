2891fd07b52f2ca4d548f4d90c3f17c858297dc0
nấm <87581534+phamthimylinhhaui@users.noreply.github.com>
2022-06-26 15:15:25 +0700

    Revert "[#3] modify: header and banner responsive"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="banner">
-        <div class="container-banner">
-            <div class="content">
-                <div class="content-item">learn anytime, anywhere</div>
-                <div class="content-title"><span>at HapoLearning</span>
-                    <img src="{{ asset('images/hapo.png') }}" alt="icon-hapo">!
-                </div>
-                <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
-                <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
-            </div>
-        </div>
-        <div class="effect-banner"></div>
-    </section>
 
+<section class="banner">
+    <div class="content">
+        <div class="content-title-1 content-item">learn anytime, anywhere</div>
+        <div class="content-title-2 content-item">at HapoLearning
+            <img src="http://localhost/scaford-hapolearn/resources/assets/images/hapo.png">
+            !
+        </div>
+        <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
+        <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
+    </div>
+</section>
+{{--<section class="example">--}}
+{{--    <div class="container">--}}
+{{--        <div class="row justify-content-center">--}}
+{{--            <div class="col-md-12">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-header">{{ __('Dashboard') }}</div>--}}
+{{--                    <div class="card-body">--}}
+{{--                        Hello World!!!--}}
+{{--                    </div>--}}
+
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</section>--}}
+{{--<div class="hello">--}}
+{{--    đây là test thử--}}
+{{--</div>--}}
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <header class="main-header">
-    <nav class="navbar navbar-expand-lg navbar-light ">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
             <img class="logo-image" src="{{ asset('images/logo.png') }}" alt="HapoLearn Logo">
         </a>
@@ -9,18 +9,30 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto w-100">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">All Courses</a>
+                    <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">Login/Register</a>
+                    <a class="nav-link " href="#">ALL COURSE</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="#">Profile</a>
+                    <a class="nav-link" href="#">LOGIN/REGISTER</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PROFILE</a>
+                </li>
+{{--                <li class="nav-item dropdown">--}}
+{{--                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                        LOGIN/REGISTER--}}
+{{--                    </a>--}}
+{{--                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
+{{--                        <a class="dropdown-item" href="#">Action</a>--}}
+{{--                        <a class="dropdown-item" href="#">Another action</a>--}}
+{{--                        <div class="dropdown-divider"></div>--}}
+{{--                        <a class="dropdown-item" href="#">Something else here</a>--}}
+{{--                    </div>--}}
+{{--                </li>--}}
+
             </ul>
         </div>
     </nav>
