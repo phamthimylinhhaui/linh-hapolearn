c6524e1da1cd05decdb63e24342b7d3e3a0c43ae
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-27 15:17:30 +0700

    #12: fix EOF

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,7 @@
     <div class="content">
         <div class="content-title-1 content-item">learn anytime, anywhere</div>
         <div class="content-title-2 content-item">at HapoLearning
-            <img src="http://localhost/scaford-hapolearn/resources/assets/images/hapo.png">
-            !
+            <img src="http://localhost/scaford-hapolearn/resources/assets/images/hapo.png">!
         </div>
         <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
         <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
